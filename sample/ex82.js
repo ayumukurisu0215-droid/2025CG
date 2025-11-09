97273aa782a029d5d6088cc66eade3a3a200a42a
ayumu <ayumu.jp>
2025-11-09 17:03:33 +0900

    add CG_Calculation

--- sample/ex82.js
+++
@@ -1,6 +0,0 @@
-document.title = "JavaScript 例２";
-document.body.style.backgroundColor = "yellow";
-
-document.writeln("背景色を設定");
-
-document.writeln("<p>背景色:",document.body.style.backgroundColor,"</p>");
